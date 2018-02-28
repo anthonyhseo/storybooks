65f144e20f3ebb3176470214707881b54ad9ae8e
unknown <anthonyhseo@gmail.com>
2018-02-28 13:32:37 -0800

    finished initial development and added private keys

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
-// Load User Model
+// Load Models
 require('./models/User');
+require('./models/Story');
 
 // Passport Config
 require('./config/passport')(passport);
@@ -14,9 +18,19 @@ require('./config/passport')(passport);
 // Load routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
 // Load Keys
 const keys = require('./config/keys');
+
+// Handlebars Helpers
+const {
+  truncate,
+  stripTags,
+  formatDate,
+  select,
+  editIcon
+} = require('./helpers/hbs');
 
 // Map global promises
 mongoose.Promise = global.Promise;
@@ -30,8 +44,25 @@ mongoose.connect(keys.mongoURI, {
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Body parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Method Override Middleware
+app.use(methodOverride('_method'));
+
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
+  helpers: {
+    truncate,
+    stripTags,
+    formatDate,
+    select,
+    editIcon
+  },
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
@@ -54,9 +85,13 @@ app.use((req, res, next) => {
   next();
 });
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Use Routes
 app.use('/', index);
 app.use('/auth', auth);
+app.use('/stories', stories);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
